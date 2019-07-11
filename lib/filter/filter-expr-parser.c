23c71a89818ceb475b638d89ff93e9679a304f67
Kokan <kokaipeter@gmail.com>
2019-07-11 14:31:23 +0200

    filter: standalone filter parser missed KW_TEMPLATE
    Signed-off-by: Kokan <kokaipeter@gmail.com>

--- lib/filter/filter-expr-parser.c
+++ lib/filter/filter-expr-parser.c
@@ -64,6 +64,7 @@ static CfgLexerKeyword filter_expr_keywords[] =
   { "value",              KW_VALUE },
   { "flags",              KW_FLAGS },
   { "type",              KW_TYPE },
+  { "template",              KW_TEMPLATE },
 
   { NULL }
 };
